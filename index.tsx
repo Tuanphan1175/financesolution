384de871c7383cf887eec9163ed21096691f02dc
Tuanphan <Tulovy2007@gmail.com>
2026-01-23 15:45:56 +0700

    Fix formatting of error message in index.tsx

--- index.tsx
+++ index.tsx
@@ -1,12 +1,11 @@
-// index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthGate } from './AuthGate'; // ⭐ QUAN TRỌNG
+import { AuthGate } from './AuthGate';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
