c2eee74131f22a610a1073779f178090694b971a
Tuanphan <Tulovy2007@gmail.com>
2026-01-26 11:24:47 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import { AuthGate } from "./AuthGate";
+import AuthGate from "./AuthGate";
 
 // ====== LẤY ROOT ELEMENT ======
 const container = document.getElementById("root");
